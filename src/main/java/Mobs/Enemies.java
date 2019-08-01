6dc02a7a8c90d0f13658277fd83a7c3c89d744c4
John McNeill <Shetlandjohnmcneill@hotmail.co.uk>
2019-08-01 11:14:10 +0100

    added mobs folder and enemies class

---
+++ src/main/java/Mobs/Enemies.java
@@ -0,0 +1,28 @@
+package Mobs;
+
+public class Enemies {
+
+    private int HP;
+    private int attackDMG;
+    private String type;
+
+    public Enemies(int HP, int attackDMG, String type) {
+        this.HP = HP;
+        this.attackDMG = attackDMG;
+        this.type = type;
+    }
+
+    public int getHP() {
+        return this.HP;
+    }
+
+    public int getAttackDMG() {
+        return this.attackDMG;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+
+}
