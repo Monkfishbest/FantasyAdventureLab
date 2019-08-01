110e0842730401bf807dd78aa04ca882f8c285bb
Kyle Walker <Randomer44@hotmail.com>
2019-08-01 10:50:31 +0100

    Character class setup

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,2 +1,13 @@
-public class Character {
+public abstract class Character {
+    private String name;
+    private int HealthPoints;
+    private String weapon;
+    private int inventory;
+
+    public Character(String name, int healthPoints, String weapon) {
+        this.name = name;
+        HealthPoints = healthPoints;
+        this.weapon = weapon;
+        this.inventory = 0;
+    }
 }
