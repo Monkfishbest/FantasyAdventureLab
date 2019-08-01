1ff828a251fe94006c85998b58c19c28a260905e
Kyle Walker <Randomer44@hotmail.com>
2019-08-01 11:20:29 +0100

    Spellcaster class created

--- src/main/java/Characters/SpellCaster.java
+++ src/main/java/Characters/SpellCaster.java
@@ -1,7 +1,23 @@
 package Characters;
 
 import Enums.Spell;
+import Enums.Weapon;
 
-public class SpellCaster {
-    protected Spell spell
+public class SpellCaster extends Character {
+    protected Spell spell;
+    private String summon;
+
+    public SpellCaster(String name, Weapon weapon, Spell spell, String summon) {
+        super(name, 50, weapon);
+        this.spell = spell;
+        this.summon = summon;
+    }
+
+    public void changeSpell(Spell spell) {
+        this.spell = spell;
+    }
+
+    public void changeSummon(String summon){
+        this.summon = summon;
+    }
 }
